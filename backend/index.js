c2dbca304c8d2a04be3f44f408e184e353105c83
chandamin <chandamin084@gmail.com>
2026-01-14 10:16:57 +0530

    CORS Policy

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ const connectDB = require('./db/mongo');
  * ---------------------------------------
  */
 app.use(cors({
-    origin: [process.env.FRONTEND_URL, 'https://unpenciled-unhumored-thora.ngrok-free.dev'],
+    origin: [process.env.FRONTEND_URL, 'https://unpenciled-unhumored-thora.ngrok-free.dev', 'https://airwall.kaswebtechsolutions.com'],
     credentials: true,
 }));
 
