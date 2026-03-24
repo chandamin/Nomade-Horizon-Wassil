0c0ac107bdd5673b6fd099be134986857d4d67d8
chandamin <chandamin084@gmail.com>
2026-03-24 17:44:37 +0530

    CORS changes

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,8 @@ app.use(cors({
     process.env.FRONTEND_URL,
     process.env.BACKEND_URL,
     'https://airwall.kaswebtechsolutions.com',
-    'http://localhost:5173'
+    'http://localhost:5173',
+    'checkout.nomade-horizon.com',
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
