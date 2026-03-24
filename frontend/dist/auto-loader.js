ff9f1ee6b4d269c527a44447349e6dc2e7f85195
chandamin <chandamin084@gmail.com>
2026-03-24 16:52:16 +0530

    vite config changes

--- frontend/dist/auto-loader.js
+++
@@ -1,1 +0,0 @@
-import{c as e,j as o,C as c}from"./assets/Checkout-B3LngGDG.js";const t=document.createElement("div");t.id="custom-checkout-root";document.body.appendChild(t);e.createRoot(t).render(o.jsx(c,{}));
--- frontend/vite.config.mjs
+++ frontend/vite.config.mjs
@@ -15,17 +15,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/Nomade-Horizon-Wassil/frontend/',
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        'auto-loader': '/src/auto-loader.jsx',
-        main: '/src/index.jsx'
-      },
-      output: {
-        entryFileNames: '[name].js'
-      }
-    }
-  },
   server: {
     allowedHosts: ['.ngrok-free.dev'],
     hmr: false,
